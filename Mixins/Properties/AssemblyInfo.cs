a7f7087024eaf0a595c5adf610817f9a3f10fef2
Sergey Klementiev <sklementiev@hotmail.com>
2016-02-19 17:28:40 +1100

    package title change to enable upload to NuGet

--- Mixins/Properties/AssemblyInfo.cs
+++ Mixins/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Mixins")]
+[assembly: AssemblyTitle("Mixins.NET")]
 [assembly: AssemblyDescription("Mixins implementation in .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sergey Klementiev")]
